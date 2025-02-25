41402d39f5de776ffbdaca3b8f8012d36307dd0c
Cen2grade <165027415+Cen2grade@users.noreply.github.com>
2025-02-25 09:11:43 -0600

    Add files via upload

---
+++ filepractice.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <windows.h>
+#include <WinDef.h>
+//   | required thing for windows api |
+#ifdef _UNICODE
+#define _tcslen     wcslen
+#else
+#define _tcslen     strlen
+#endif 
+//
+#define FALSE    0 
+#define TRUE     1
+//
+
+using namespace std;
+
+int main(){
+
+}
