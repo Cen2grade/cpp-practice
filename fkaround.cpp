64b4f5d27d917a2e4a164b9666e9457ac7c74e3b
Cen2grade <165027415+Cen2grade@users.noreply.github.com>
2025-02-25 09:12:29 -0600

    Add files via upload

---
+++ fkaround.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+//global vars
+
+//end global vars
+
+
+// int main
+int main(){
+
+}
+// end main
+
+
+// Functions
+
+// End Functions
+
+
+
+/* NOTES
+
+
+*/
